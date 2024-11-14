4eec58511e277954abdeba1940faf1e42dc343e6
Kittikhong Chanthakorn <kityakuza@gmail.com>
2024-11-14 22:23:07 +0700

    Answer:Assignment : JavaScript Callbacks and Higher Order Functions

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,20 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  const scorepasseds = array.filter(score => score > 70).length;
+  return scorepasseds >= operation ? 'ผ่านเกณฑ์ ✅' : 'ไม่ผ่านเกณฑ์ ❌';
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+let roomPassing = 5;
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, roomPassing);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, roomPassing);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, roomPassing);
+
+console.log(`นักเรียนห้องที่ 1 ${scoreRoom1Result}`);
+console.log(`นักเรียนห้องที่ 2 ${scoreRoom2Result}`);
+console.log(`นักเรียนห้องที่ 3 ${scoreRoom3Result}`);
